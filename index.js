0ac712129cd82c7e2842e86aefa88d0777f54e6f
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-05-01 22:17:19 +0100

    new save

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function showmenu(){
+function showMenu(){
     const menu = document.querySelector(".menulinks");
     const icon = document.querySelector(".responsiveicon");
+    console.log(menu);
+    console.log(icon);
     menu.classList.toggle("show");
     icon.classList.toggle("show");
 }
