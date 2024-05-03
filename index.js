edbc581b3e7a3715476b0cf93b1063f7650b9729
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-05-03 20:49:08 +0100

    .

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
         if(entry.isIntersecting){
-            entry.target.classList.add("show");
+            entry.target.classList.add("nothidden");
         }
         else{
-            entry.target.classList.remove("show");
+            entry.target.classList.remove("nothidden");
         }
     });
 });
