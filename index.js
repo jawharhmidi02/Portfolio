9667927cc59a969d9870ad235750f8a55c3042c8
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-05-03 02:21:58 +0100

    .

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function showMenu(){
     menu.classList.toggle("show");
     icon.classList.toggle("show");
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
