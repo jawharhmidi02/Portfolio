811467d5d5b85e9e182d208add01e44b7368e771
Jawhar Hmidi <jawharhmidi02@gmail.com>
2025-01-29 21:39:06 +0100

    asd

--- index.js
+++ index.js
@@ -2,34 +2,31 @@ document.body.style.overflow = "hidden";
 const hiddenElements = document.querySelectorAll(".hidden1");
 hiddenElements[0].classList.add("show1");
 
-setTimeout(function(){
-    document.querySelector(".welcomepage").classList.add("pagehidden");
-    document.querySelector(".page").classList.remove("page");
-    document.body.style.overflow = "";
-    document.querySelector(".welcomepage").style.display = "none";
-    
+setTimeout(function () {
+  document.querySelector(".welcomepage").classList.add("pagehidden");
+  document.querySelector(".page").classList.remove("page");
+  document.body.style.overflow = "";
+  document.querySelector(".welcomepage").style.display = "none";
 }, 3000);
 
-
-function showMenu(){
-    const menu = document.querySelector(".menulinks");
-    const icon = document.querySelector(".responsiveicon");
-    console.log(menu);
-    console.log(icon);
-    menu.classList.toggle("show");
-    icon.classList.toggle("show");
+function showMenu() {
+  const menu = document.querySelector(".menulinks");
+  const icon = document.querySelector(".responsiveicon");
+  console.log(menu);
+  console.log(icon);
+  menu.classList.toggle("show");
+  icon.classList.toggle("show");
 }
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("nothidden");
-        }
-        // else{
-        //     entry.target.classList.remove("nothidden");
-        // }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("nothidden");
+    }
+    // else{
+    //     entry.target.classList.remove("nothidden");
+    // }
+  });
 });
 const hiddenElement = document.querySelectorAll(".hidden");
 hiddenElement.forEach((el) => observer.observe(el));
-
